16a595283bd7c0fed8386bd094a19c782699727a
Dima Kovalkov <vysserath@gmail.com>
2024-04-16 11:45:27 +0300

    services fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,31 @@
-const info = document.querySelectorAll(".service__info");
+var acc = document.getElementsByClassName("service__button");
+var i;
 
-info.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("active");
-    })
-})
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+
+    for (var j = 0; j < acc.length; j++) {
+      if (acc[j] !== this) {
+        acc[j].classList.remove("active");
+        var otherPanel = acc[j].nextElementSibling;
+        otherPanel.style.maxHeight = null;
+        acc[j].style.borderColor = "";
+        if (acc[j].nextElementSibling) {
+          acc[j].nextElementSibling.style.borderColor = "";
+        }
+      }
+    }
+
+    this.classList.toggle("active");
+    if (panel) {
+      panel.style.borderColor = this.classList.contains("active") ? "red" : ""; 
+    }
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
